95c660f401a1f1d4ba2cfd516a1f385c01114fb6
BlackSpirits <blackspirits@gmail.com>
2026-08-20 23:39:00 +0200

    Test private message report visibility

---
+++ test/integration/report_message_privacy_test.rb
@@ -0,0 +1,31 @@
+require 'test_helper'
+
+class ReportMessagePrivacyTest < ActionDispatch::IntegrationTest
+  include Devise::Test::IntegrationHelpers
+
+  setup do
+    @message = messages(:geoff_and_junior_1)
+    @report = Report.create!(
+      item: @message,
+      reporter: users(:junior),
+      reason: Report::REASON_SPAM,
+      explanation_markup: 'markdown'
+    )
+  end
+
+  test 'message report is not public' do
+    get report_path(@report)
+
+    assert_response :not_found
+    assert_not_includes response.body, @message.content
+  end
+
+  test 'conversation participant can view message report' do
+    sign_in users(:geoff)
+
+    get report_path(@report)
+
+    assert_response :success
+    assert_includes response.body, @message.content
+  end
+end
